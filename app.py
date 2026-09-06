c2908a70de232f7f1db224150f456d76db8e26de
NISHKARSH-017 <amitm62904@gmail.com>
2026-09-07 00:23:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from chart_processor import load_chart, resize_image
 from modules.visualization import create_accessible_bar_chart
 from modules.scoring import calculate_accessibility_score, get_score_color, get_score_message
 from modules.ai import generate_explanation
-from color_analyzer import get_dominant_colors
+from color_analyzer import get_dominant_colors, scan_for_accessibility_issues
 
 # ---------------------------------------------------------
 # PAGE CONFIGURATION & STYLING
